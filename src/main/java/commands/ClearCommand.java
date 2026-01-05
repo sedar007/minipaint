66db25c89bc6ae139176fd30d1a48407df81787e
sedar007 <dev@adjysedar.fr>
2026-01-05 13:57:38 +0100

    fix

---
+++ src/main/java/commands/ClearCommand.java
@@ -0,0 +1,37 @@
+package commands;
+
+import shapes.DrawingPane;
+import shapes.IShape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClearCommand  implements ICommand {
+
+    private final DrawingPane drawingPane;
+    private List<IShape> savedShapes;
+
+    public ClearCommand(DrawingPane drawingPane) {
+        this.drawingPane = drawingPane;
+    }
+
+    @Override
+    public void execute() {
+        if(drawingPane == null)
+            return;
+
+        for(IShape shape : drawingPane)
+            this.savedShapes.add(shape);
+
+        this.drawingPane.clear();
+    }
+
+    @Override
+    public void undo() {
+        if(drawingPane == null || savedShapes == null) return;
+        this.drawingPane.clear();
+
+        for(IShape shape : savedShapes)
+            this.drawingPane.addShape(shape);
+    }
+}
---
+++ src/main/java/commands/ICommand.java
@@ -0,0 +1,8 @@
+package commands;
+
+public interface ICommand {
+    public void execute();
+    public void undo();
+
+}
+
